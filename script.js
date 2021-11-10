89744770cd4b19678bdd0a7d9f4cd95547d29553
Karoline KB <79204560+karolinekb05@users.noreply.github.com>
2021-11-10 12:35:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+    function slider(anything){
+        document.querySelector('.floricultura').src = anything
+    }
+
+    function changeColor(color){
+        const circle = document.querySelector('.circle');
+        circle.style.background = color;
+    }
